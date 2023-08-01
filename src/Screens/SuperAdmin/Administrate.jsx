61e9ee366d9c6dadb3d9c697f8efe7c06aeba588
Dhruv-Darji <gauravkumar1304@gmail.com>
2023-08-01 22:31:04 +0530

    Super Admin Role Assiging Done with backend

--- src/Screens/SuperAdmin/Administrate.jsx
+++ src/Screens/SuperAdmin/Administrate.jsx
@@ -6,6 +6,10 @@ import axios from "axios";
 
 const Administrate = () => {
     const navigate = useNavigate();
+    const [selectedRole,setSelectedRole] = useState({
+        RoleId:'',
+        RoleName:'',       
+    });
     const [users,SetUsers] = useState([]);
     const [userAuth, setUserAuth] = useState({
         RoleId: '',
@@ -32,6 +36,29 @@ const Administrate = () => {
         fetchData();
         fetchUser();
     }, []);
+
+    const handleRoleChange = (user) =>{
+        const changeRoleId = selectedRole.RoleId;
+        const userUserId = user.UserId;
+        const currentRoleId = userAuth.RoleId;
+        const combinedData = {changeRoleId,userUserId,currentRoleId};
+        console.log("formData:",combinedData)
+        if(window.confirm(`Are You Sure to assign him as ${selectedRole.RoleName}`)){
+            axios.post('http://192.168.0.112:8080/change/assignRole',combinedData).then(
+                ()=>{
+                    console.log("Role Updated successfuly");
+                    fetchUser();
+                    setSelectedRole({
+                        RoleId:'',
+                        RoleName:'',    
+                    });
+                }
+            )
+        }
+        else{
+            console.log('You have blocked the notification of window.confirm')
+        }      
+    }
 
     const columnAndRow ={
         autowidth:true,
@@ -88,69 +115,158 @@ const Administrate = () => {
                 Pincode:user.PinCode,
                 Role: user.RoleId===1 ? 'SuperAdmin' : user.RoleId===2 ? 'DistrictAdmin': user.RoleId===3 ? 'Admin': 'NormalUser', 
                 Show:(
-                    <button type="button" data-mdb-toggle="modal" data-mdb-target="#exampleModal" className="btn btn-primary btn-floating">
-                        <i className="fas fa-download"></i>
+                    <button type="button" data-mdb-toggle="modal" data-mdb-target={`#exampleModal-${index}`} className="btn btn-secondary btn-floating">
+                        <i className="fas fa-pen-to-square"></i>
                     </button>          
                 )
             }
         })
     }
-    if (userAuth.RoleId === 1 || userAuth.RoleId === 2) {
-        return (
-            <>
-            <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div className="modal-dialog">
+
+    const ProfileModal = ({user}) =>{
+        if(!user){
+            return(
+                <h1>Not user</h1>
+            );
+        }
+        const datePart = new Date(user.DOB).toLocaleDateString();
+        return (        
                 <div className="modal-content">
                 <div className="modal-header">
-                    <h5 className="modal-title" id="exampleModalLabel">Edit</h5>
-                    <button type="button" className="btn-close" data-mdb-dismiss="modal" aria-label="Close"></button>
+                    <h5 className="modal-title" id="exampleModalLabel">Edit In Role</h5>
+                    <button type="button" onClick={()=>setSelectedRole({
+                        RoleId:'',
+                        RoleName:'',       
+                    })} className="btn-close" data-mdb-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div className="modal-body">
                 <div className="card-body text-center">
             <div className="mt-3 mb-4">
-              <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava2-bg.webp"
+              <img src="Images/Profile.jpeg"
+                alt="test"
                 className="rounded-circle img-fluid w-25"/>
             </div>
-            <h4 className="mb-2">Julie L. Arsenault</h4>
-            <p className="text-muted mb-4">@Programmer <span className="mx-2">|</span> <a
-                href="#!">mdbootstrap.com</a></p>
-            <div className="mb-4 pb-2">
-              <button type="button" className="btn btn-outline-primary btn-floating">
-                <i className="fab fa-facebook-f fa-lg"></i>
-              </button>
-              <button type="button" className="btn btn-outline-primary btn-floating">
-                <i className="fab fa-twitter fa-lg"></i>
-              </button>
-              <button type="button" className="btn btn-outline-primary btn-floating">
-                <i className="fab fa-skype fa-lg"></i>
-              </button>
-            </div>
+            <h4 className="mb-2">{`${user.SurName} ${user.MiddleName} ${user.FatherName}`}</h4>
+            <p className="text-muted mb-4">
+            {(()=>{
+                if(user.RoleId===1){
+                    return(
+                        <span className="badge badge-success rounded-pill d-inline">@SuperAdmin</span>
+                    );
+                }
+                else if(user.RoleId===2){
+                    return(
+                        <span className="badge badge-primary rounded-pill d-inline">@DistrictAdmin</span>
+                    );
+                }
+                else if(user.RoleId===3){
+                    return(
+                        <span className="badge badge-warning rounded-pill d-inline">@Admin</span>
+                    );
+                }else{
+                    return(
+                        <span className="badge badge-danger rounded-pill d-inline">@NormalUser</span>
+                    );
+                }
+            })()}
+            <span className="mx-2">|</span> <a
+                href="#!">{`${user.Email}`}</a>
+            <span className="mx-2">|</span>
+            {`${datePart}`} 
+                </p>            
             <button type="button" className="btn btn-primary btn-rounded btn-lg">
               Message now
             </button>
-            <div className="d-flex justify-content-between text-center mt-5 mb-2">
+            <div className="card-body p-4">
+                <hr className="mt-0 mb-4"/>
+                <div className="row pt-1">
+                  <div className="col-6 mb-3">
+                    <p className="text-muted fw-bold mb-0">Village</p>
+                    <p className="mb-2 fw-light">{`${user.Village}`}</p> 
+                  </div>
+                  <div className="col-6 mb-3">
+                    <p className="text-muted fw-bold mb-0">Address</p>
+                    <p className="mb-2 fw-light">{`${user.Address}`}</p> 
+                  </div>
+                </div>
+                </div>
+            <div className="d-flex justify-content-between text-center p-4 mt-2 mb-2">
               <div>
-                <p className="mb-2 h5">8471</p>
-                <p className="text-muted mb-0">Wallets Balance</p>
+                <p className="text-muted fw-bold mb-0">Gender</p>
+                <p className="mb-2 fw-light">{`${user.Gender}`}</p>                
               </div>
               <div className="px-3">
-                <p className="mb-2 h5">8512</p>
-                <p className="text-muted mb-0">Income amounts</p>
+                <p className="text-muted fw-bold mb-0">Pargana</p>
+                <p className="mb-2 fw-light">{`${user.Pargana}`}</p>                
               </div>
               <div>
-                <p className="mb-2 h5">4751</p>
-                <p className="text-muted mb-0">Total Transactions</p>
+                <p className="text-muted fw-bold mb-0">Native</p>
+                <p className="mb-2 fw-light">{`${user.Native}`}</p>                
               </div>
             </div>
           </div>
                 </div>
                 <div className="modal-footer">
-                    <button type="button" className="btn btn-secondary" data-mdb-dismiss="modal">Close</button>
-                    <button type="button" className="btn btn-primary">Save changes</button>
+                {(()=>{
+                    if(!selectedRole.RoleId){if(userAuth.RoleId===1){
+                        return(
+                            <p>Make him{" "}
+                            <span onClick={()=>setSelectedRole({RoleId:2,RoleName:'DistrictAdmin'})} className="badge badge-primary rounded-pill d-inline">@DistrictAdmin</span>
+                            {" "}or{" "}
+                            <span onClick={()=>setSelectedRole({RoleId:3,RoleName:'Admin'})} className="badge badge-warning rounded-pill d-inline">@Admin</span>                            
+                            </p>
+                        );
+                    }
+                    if(userAuth.RoleId===2){
+                        return(
+                            <p>Make him{" "}                            
+                            <span onClick={()=>setSelectedRole({RoleId:3,RoleName:'Admin'})} className="badge badge-warning rounded-pill d-inline">@Admin</span>                            
+                            </p>
+                        );                        
+                    }}
+                    else{
+                        if(selectedRole.RoleId===2){
+                            return (
+                                <p>You Assigning Role Of <span role="img" aria-label="right-hand">👉</span> {" "}
+                                <span className="badge badge-primary rounded-pill d-inline">@DistrictAdmin</span>
+                                </p>
+                            );
+                        }
+                        if(selectedRole.RoleId===3){
+                            return(
+                                <p>You Assigning Role Of <span role="img" aria-label="right-hand">👉</span> {" "}
+                                <span className="badge badge-warning rounded-pill d-inline">@Admin</span>
+                                </p>
+                            );
+                        }                        
+                    }
+                })()}
+                    <button type="button" onClick={()=>setSelectedRole({RoleId:'',RoleName:''})} className="btn btn-secondary" data-mdb-dismiss="modal">Close</button>
+                    { !selectedRole.RoleId?<></>:(<button type="button" onClick={()=>handleRoleChange(user)} data-mdb-dismiss="modal" className="btn btn-secondary">Save changes</button>)}
                 </div>
+                </div>);
+    }
+
+    if (userAuth.RoleId === 1 || userAuth.RoleId === 2) {
+        return (
+            <> 
+            {users.map((user, index) => (
+                <div
+                    className="modal fade"
+                    id={`exampleModal-${index}`}
+                    tabIndex="-1"
+                    aria-labelledby="exampleModalLabel"
+                    aria-hidden="true"
+                    key={index}
+                >
+                    <div
+                    className="modal-dialog"
+                    style={{ maxWidth: "50%" }}
+                    >
+                    <ProfileModal user={user} />
+                    </div>
                 </div>
-            </div>
-            </div>
+            ))}           
             <div className="row justify-content-center">
             <div className="card text-center w-75 mt-4">
             <MDBContainer>
