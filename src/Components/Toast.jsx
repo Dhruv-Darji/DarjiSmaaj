4d9dded420ba2fb211e0ad907d20d5f60ff8381d
Dhruv-Darji <gauravkumar1304@gmail.com>
2023-08-07 21:45:01 +0530

    react-toastify is successfully added

---
+++ src/Components/Toast.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const Toast = () => {
+  return (
+    <div>
+      {/* Toast container component */}
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        closeOnClick
+        pauseOnHover
+        draggable
+      />
+    </div>
+  );
+};
+
+export const showSuccessToast = (message) => {
+  toast.success(message, {
+    position: 'top-right',
+    autoClose: 2000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+  });
+};
+
+export const showErrorToast = (message) => {
+  toast.error(message, {
+    position: 'top-right',
+    autoClose: 2000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+  });
+};
+
+export default Toast;
--- src/Screens/SuperAdmin/DistrictAdminList.jsx
+++ src/Screens/SuperAdmin/DistrictAdminList.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import Toast, { showSuccessToast } from '../../Components/Toast';
 
-const DistrictAdminList = () =>{
-    return(
-        <>
-            <h1>This is the DistrictAdminList</h1>
-        </>
-    );
-}
+const DistrictAdminList = () => {
+
+  return (
+    <>
+     <Toast/>
+      <button onClick={()=>showSuccessToast('this is the test')} className="btn btn-secondary">test</button>
+    </>
+  );
+};
 
 export default DistrictAdminList;
